c00de8748291445c424500188aca02c04c3fce4b
Juan Francisco Rodriguez <rodriguezjf2006@gmail.com>
2026-04-29 16:20:42 -0300

    Se sube el archivo de validacion con la funcion de validar las coordenadas

---
+++ src/validacion.py
@@ -0,0 +1,77 @@
+
+"""
+    En este modulo se encuentran las funciones de validacion de los datos del dataset
+"""
+from pathlib import Path
+import csv
+import os
+
+
+
+def ruta(dataset,archivo):
+
+    """Funcion que obtiene la ruta hacia el dataset original"""
+
+    raiz=Path(__file__).parent.parent
+    rute_in = Path(os.path.join(raiz, 'raw_datasets', dataset, archivo))
+    if not rute_in.exists():
+        print(f'el archivo {rute_in} no existe')
+        return None
+    return rute_in
+
+def latitud(latitude):
+
+    """Funcion que valida la latitud de un registro"""
+
+    if(latitude is None or latitude.strip() == ""):
+        return False
+    else:
+        latitude=float(latitude)
+        if(latitude<-90 or latitude>90):
+            return True
+        else:
+            return False
+    
+def longitud(longitude):
+
+    """Funcion que valida la longitud de un registro"""
+
+    if(longitude is None or longitude.strip() == ""):
+        return False
+    else:
+        longitude=float(longitude)
+        if(longitude<-180 or longitude>180):
+            return True
+        else:
+            return False
+
+def coordenadas(dataset,archivo,delimitadror=","):
+
+    """Valida la informacion de los campos decimalLatitude|latitudeDecimal y decimalLongitude|longitudeDecimal, y retorna 
+       cantidad de registros invalidos y los registros con coordenadas invalidas
+    """
+
+    rute_in=ruta(dataset,archivo)
+    if not rute_in:
+        return None
+    with open (rute_in,'r',encoding='utf-8') as archivo:
+        cant_invalidos=0
+        registros_incorrectos=[]
+        datos=csv.DictReader(archivo,delimiter=delimitadror)
+        latitude=[datos for datos in datos.fieldnames if "latitude" in datos.lower()][0]
+        longitude=[datos for datos in datos.fieldnames if "longitude" in datos.lower()][0]
+        print(dataset)
+        for fila in datos:
+            if(latitud(fila[latitude])):
+                cant_invalidos+=1
+                registros_incorrectos.append(fila)
+            if(longitud(fila[longitude])):
+                cant_invalidos+=1
+                registros_incorrectos.append(fila)
+
+        print(f"en el dataset: {dataset} hay {cant_invalidos} coordenadas invalidas {registros_incorrectos}")
+        print(" \n")
+
+    return registros_incorrectos, cant_invalidos
+
+    
