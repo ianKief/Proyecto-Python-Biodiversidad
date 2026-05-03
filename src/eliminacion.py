44c5f8e1c5e88c1c4c53769b857e085bd7fb3d51
Juan Francisco Rodriguez <rodriguezjf2006@gmail.com>
2026-05-03 20:41:56 -0300

    Se agrego la funcion de identificar el registro a eliminar mediante el ocurrenceID

--- src/eliminacion.py
+++ src/eliminacion.py
@@ -2,6 +2,40 @@ import csv
 import logger
 from lectura import obtener_ruta
 
+def identificador(dataset,archivo,valorID,delimitador=","):
+
+    """Funcion que recibe en valorID el ID a eliminar"""
+
+    rute_in,rute_out=obtener_ruta(dataset,archivo)
+    with open(rute_in,"r",encoding="utf-8") as archivo:
+        datos=csv.DictReader(archivo,delimiter=delimitador)
+        campos=datos.fieldnames
+        campoID=campos[0]
+        registros=[]
+        cant=0
+        for fila in datos:
+            if(fila[campoID] is None or fila[campoID].strip()==""):
+                continue
+            else:
+                if (fila[campoID]==valorID):
+                    cant+=1
+                else:
+                    registros.append(fila)
+
+    if cant==0:
+        logger.log_error(dataset,"DELETE")
+        return (f"ERROR, no se encontro un registro con ID: {valorID}")
+    
+    #Si se encontro el ID a eliminar guarda el archivo actualizado
+    with open(rute_out,"w", encoding="utf-8") as actualizado:
+        writer=csv.DictWriter(actualizado,fieldnames=campos,delimiter=",")
+        writer.writeheader()
+        writer.writerows(registros)
+
+    logger.log(dataset,"DELETE",cant)
+
+    return "Se eliminaron los registros requeridos"
+
 
 def eliminar_columna(dataset,archivo,columna,valores,delimitador=","):
 
@@ -27,9 +61,9 @@ def eliminar_columna(dataset,archivo,columna,valores,delimitador=","):
     
     #Si se elimino algun registro se guarda el archivo actualizado con los registros eliminados
     with open(rute_out,"w",encoding="utf-8") as actualizado:
-        reader=csv.DictWriter(actualizado,fieldnames=campos,delimiter=",")
-        reader.writeheader()
-        reader.writerows(registros)
+        writer=csv.DictWriter(actualizado,fieldnames=campos,delimiter=",")
+        writer.writeheader()
+        writer.writerows(registros)
 
     logger.log(dataset,"DELETE",cant)
 
