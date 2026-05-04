9732da4e4be88d5666a97fd2fb37164ab88bf533
Juan Francisco Rodriguez <rodriguezjf2006@gmail.com>
2026-05-03 22:12:24 -0300

    Agregada la funcion para verificar una condicion pasada por parametro

--- src/eliminacion.py
+++ src/eliminacion.py
@@ -69,3 +69,57 @@ def eliminar_columna(dataset,archivo,columna,valores,delimitador=","):
 
     return "Se eliminaron los registros requeridos"
 
+
+def condicion(dataset,archivo,columnas,valor,condicion,delimitador=","):
+
+    """Funcion que recibe una lista de columnas a verificar, la condicion a cumplir y el valor por parametros,
+       y elimina los registros que cumplan la condicion"""
+
+    rute_in,rute_out=obtener_ruta(dataset,archivo)
+    cant=0
+    registros=[]
+
+    condiciones={
+        "==":lambda a, b: a == b,
+        "!=":lambda a, b: a!=b,
+        ">":lambda a, b: a > b,
+        ">=":lambda a, b: a >= b,
+        "<":lambda a, b: a < b,
+        "<=":lambda a, b: a <= b
+    }
+
+    with open(rute_in,"r",encoding="utf-8") as archivo_in:
+        datos=csv.DictReader(archivo_in,delimiter=delimitador)
+        campos=datos.fieldnames
+
+        if not(condicion in condiciones):
+            return (f" la condicion {condicion} no es valida")
+        
+        for columna in columnas:
+            if not(columna in datos.fieldnames):
+                print(f"El campo {columna} no existe")
+                continue
+            
+            for fila in datos:
+                dato=fila.get(columna)
+                if(dato is None or dato.strip()==" "):
+                    continue
+                
+                dato=float(dato)
+                if (condiciones[condicion](dato,valor)):
+                    cant+=1
+                elif not(fila in registros):
+                    registros.append(fila)
+        
+    if(cant==0):
+        return "Ningun registro cumplia la condicion"
+    
+    with open(rute_out,"w",encoding="utf-8") as actualizado:
+        writer=csv.DictWriter(actualizado,fieldnames=campos,delimiter=",")
+        writer.writeheader()
+        writer.writerows(registros)
+
+    logger.log(dataset,"DELETE",cant)
+
+    return "Se eliminaron los registros que cumplian la condicion"
+
