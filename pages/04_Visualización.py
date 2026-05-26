4ea9199d8935876ae79cfd5c22c60297d097ac85
Juan Francisco Rodriguez <rodriguezjf2006@gmail.com>
2026-05-26 17:45:14 -0300

    Se agrego el codigo que permite ver un grafico de la distribucion del dataset segun lo pedido.
    Se mejoro la creacion del grafico por años

--- pages/04_Visualización.py
+++ pages/04_Visualización.py
@@ -24,24 +24,48 @@ if (df.empty):
     st.warning("El dataset está vacío. No se pueden generar visualizaciones.")
     st.stop()
 
-fechas=pd.to_datetime(
-    df['eventDate'],
-    errors="coerce"
-)
+st.subheader("Cantidad de registros por año")
+st.write(f"Aca se muestra un grafico de la cantidad de registros por año del dataset: {st.session_state['dataset']}")
 
-excluidos=fechas.isna().sum()
+try:
+    fechas=pd.to_datetime(
+        df['eventDate'],
+        errors="coerce"
+    )
 
-validas=fechas.dropna()
-años=validas.dt.year.value_counts().sort_index()
+    excluidos=fechas.isna().sum()
 
-plt.figure(figsize=(10,5))
-plt.plot(años.index, años.values, marker='o')
+    validas=fechas.dropna()
+    años=validas.dt.year.value_counts().sort_index()
 
-plt.title("cantidad de registros por año")
+    plt.figure(figsize=(10,5))
+    plt.plot(años.index, años.values, marker='o')
 
-plt.xlabel("Año")
-plt.ylabel("Cantidad de registros")
-plt.grid(True)
-st.pyplot(plt)
+    plt.title("cantidad de registros por año")
 
-st.write(f"Se excluyeron {excluidos} registros por tener fechas no válidas o vacías.")
+    plt.xlabel("Año")
+    plt.ylabel("Cantidad de registros")
+    plt.grid(True)
+    st.pyplot(plt)
+
+    st.write(f"Se excluyeron {excluidos} registros por tener fechas no válidas o vacías.")
+except:
+    st.error("No se pudo generar la gráfica de registros por año. Asegúrate de que la columna 'eventDate' exista")
+
+st.subheader("Distribución por clase, orden o familia")
+st.write(f"Aca se muestra un grafico de la distribucion de registros por clase, orden o familia del dataset: {st.session_state['dataset']}")
+
+opcion=st.selectbox("Selecciona una columna para visualizar su distribución", ("class","order","family"))
+
+try:
+    total=df[opcion].value_counts()
+    cant=st.slider("Cantidad de datos a mostrar", min_value=1, max_value=total.shape[0], value=1)
+    datos=total.head(cant)
+
+    plt.figure(figsize=(10, 5))
+    plt.pie(datos.values,labels=datos.index,autopct="%1.1f%%")
+    plt.title(f"Distribución de registros por {opcion}")
+    st.pyplot(plt)
+    
+except Exception as e:
+    st.error(f"La columna {opcion} no existe en el dataset")
