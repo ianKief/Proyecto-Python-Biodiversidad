fe2f0077b7196039a1eb47250912677d1d843311
Juan Francisco Rodriguez <rodriguezjf2006@gmail.com>
2026-06-05 22:12:08 -0300

    Modificado el ejercicio 4.B para mejorar su uso en el siguiente ejercicio

--- pages/05_Gestion de Registros.py
+++ pages/05_Gestion de Registros.py
@@ -164,23 +164,18 @@ if st.button("Validar datos e insertar"):
 
 #Ejercicio 4.B
 
-ejemplo_id={
-    "iadiza_procesado.csv":["1660909562", "gbifID"],
-    "inaturalist_procesado.csv":["56389512", "id"],
-    "xeno-canto_procesado.csv":["572960@XC o 3954627@XC", "id"]
-}
-
 st.subheader("Buscar un registro por ID")
-registro_id=st.text_input(f"Ingrese el ID que desea buscar (ejemplo: {ejemplo_id.get(st.session_state['archivo'])[0]})", key="registro_a_eliminar")
+registro_id=st.text_input(f"Ingrese el ID que desea buscar (ejemplo: {ejemplo_id.get(st.session_state['archivo'])[0]})", key="registro_a_buscar")
 
 try:
     if st.button("Buscar Registro"):
         id_columna=ejemplo_id.get(st.session_state['archivo'])[1]
         registro_encontrado=df[df[id_columna].astype(str)==registro_id]
-        if not registro_encontrado.empty:
-            st.success("Registro encontrado:")
-            st.data_editor(registro_encontrado)
-        else:
+        if registro_encontrado.empty:
             st.warning("No se encontró ningún registro con ese ID.")
+        else:
+            st.session_state["registro_encontrado"]=registro_encontrado
+            st.success("Registro encontrado:")
+
 except Exception as e:
     st.error(f"Ocurrió un error al buscar el registro: {e}")
